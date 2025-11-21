0f8f8a41739a2da5a8599ee4fcd439e06baddd3b
TuanHai <143240768+HaiDark05@users.noreply.github.com>
2025-11-21 14:27:59 +0700

    Update firebase.js

--- back_end/config/firebase.js
+++ back_end/config/firebase.js
@@ -1,11 +1,14 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
+
+// Lấy JSON từ environment variable
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://moviesproject-16df3-default-rtdb.firebaseio.com"  // Đảm bảo đúng URL của Firebase Realtime Database
+  databaseURL: "https://moviesproject-16df3-default-rtdb.firebaseio.com"
 });
 
-const db = admin.firestore();  // Sử dụng Realtime Database
-const chat = admin.database(); 
-module.exports = { chat , db };
+const db = admin.firestore();      // Firestore
+const chat = admin.database();     // Realtime Database
+
+module.exports = { chat, db };
